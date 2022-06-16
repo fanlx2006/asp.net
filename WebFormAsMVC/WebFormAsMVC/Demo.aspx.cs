1a43a4c46929b47f0493586f4def199704bbf5b6
fanlx2006 <fanlx2006@yahoo.co.jp>
2022-06-16 22:46:59 +0900

    create asp.net folder

---
+++ WebFormAsMVC/WebFormAsMVC/Demo.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormAsMVC
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = "我是 来自于数据库的数据";
+            this.Label1.Text = str;
+        }
+    }
+}
