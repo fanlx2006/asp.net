2ab764e61d4b09f8bad61a14c660622561e2e231
fanlx2006 <fanlx2006@yahoo.co.jp>
2022-06-18 23:43:14 +0900

    20020618 23:43

--- MVCDEMO/Razor_Demo/Controllers/UserController.cs
+++ MVCDEMO/Razor_Demo/Controllers/UserController.cs
@@ -11,7 +11,28 @@ namespace Razor_Demo.Controllers
         // GET: User
         public ActionResult Index()
         {
+            //1,ViewData,需要类型转换,单个页面传值
+            //2,ViewBag ,不需要类型转换，单个页面传值
+            //3,TempData,和ViewData类似，单个页面传值也可以跨页面
+
+            List<string> list1 = new List<string>() { "yuwen","shuxue","yinyu" };
+            ViewData["myage"] = 23;
+            ViewData["myname"] = "tom";
+            ViewData["mylist"] = list1;
+
+            ViewBag.name = "jeny";
+            ViewBag.age = 23;
+            ViewBag.list = list1;
+
+            TempData["yourname"] = "hello,this`s new page";
+
             return View();
         }
+
+        public ActionResult SecondPage()
+        {
+            return View();
+        }
+
     }
 }
